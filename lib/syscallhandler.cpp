00b417bb7d2a07851e744e30c295adc764130321
noahc19 <76454253+yeric1789@users.noreply.github.com>
2023-04-24 15:11:07 -0400

    Fixed

--- lib/syscallhandler.cpp
+++ lib/syscallhandler.cpp
@@ -5,7 +5,7 @@
 
 int SyscallHandler(int arg1, int arg2, int arg3, int arg4) {
 	register long syscall_no asm ("r7");
-	// CLogger::Get ()->Write ("SyscallHandler", LogDebug, "syscall_no=0x%x.", syscall_no);
+	CLogger::Get ()->Write ("SyscallHandler", LogDebug, "syscall_no=0x%x.", syscall_no);
 
 	// TODO: Handle system calls based on syscall_no.
 
